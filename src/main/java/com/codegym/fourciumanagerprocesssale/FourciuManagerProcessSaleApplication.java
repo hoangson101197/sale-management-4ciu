b12eaa90ff2ed21892b76e0154821898b94b83b3
sonnh <hoangson.101197@gmail.com>
2019-01-09 11:00:40 +0700

    launch the project

---
+++ src/main/java/com/codegym/fourciumanagerprocesssale/FourciuManagerProcessSaleApplication.java
@@ -0,0 +1,14 @@
+package com.codegym.fourciumanagerprocesssale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourciuManagerProcessSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourciuManagerProcessSaleApplication.class, args);
+	}
+
+}
+
