8e0ccd6072e08b1bb7ab1adf9b7b77204d6746a1
sonnh <hoangson.101197@gmail.com>
2019-01-22 10:57:21 +0700

    thành phần quyền

---
+++ src/main/java/com/codegym/fourciumanagerprocesssale/model/Role.java
@@ -0,0 +1,23 @@
+package com.codegym.fourciumanagerprocesssale.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "role")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+
+    @Column(name = "role")
+    private String role;
+}
