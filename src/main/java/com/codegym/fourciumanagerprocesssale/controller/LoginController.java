bb31a9209757ad98a20a4f91a1aed14649e9548f
sonnh <hoangson.101197@gmail.com>
2019-01-24 01:48:57 +0700

    Controller xử lý /registration và sửa url trong view

--- src/main/java/com/codegym/fourciumanagerprocesssale/controller/LoginController.java
+++ src/main/java/com/codegym/fourciumanagerprocesssale/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.codegym.fourciumanagerprocesssale.controller;
 
+import com.codegym.fourciumanagerprocesssale.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,4 +16,14 @@ public class LoginController {
         modelAndView.setViewName("login/login");
         return modelAndView;
     }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView registration() {
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("login/registration");
+        return modelAndView;
+    }
+
 }
