4a6cb90de29396500bdf1bc1013baac7fa014c21
sonnh <hoangson.101197@gmail.com>
2019-01-24 02:20:44 +0700

    Đăng ký được tài khoản

--- src/main/java/com/codegym/fourciumanagerprocesssale/controller/LoginController.java
+++ src/main/java/com/codegym/fourciumanagerprocesssale/controller/LoginController.java
@@ -1,13 +1,20 @@
 package com.codegym.fourciumanagerprocesssale.controller;
 
 import com.codegym.fourciumanagerprocesssale.model.User;
+import com.codegym.fourciumanagerprocesssale.service.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+
 @Controller
 public class LoginController {
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
     // Xử lý url / và /login có request tới medthod là GET  bởi method login()
@@ -23,6 +30,25 @@ public class LoginController {
         User user = new User();
         modelAndView.addObject("user", user);
         modelAndView.setViewName("login/registration");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userDetailsService.findUserByEmail(user.getEmail());
+        if  (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user", "There is already a user registered with the email provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("login/registration");
+        } else {
+            userDetailsService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("login/registration");
+        }
         return modelAndView;
     }
 
