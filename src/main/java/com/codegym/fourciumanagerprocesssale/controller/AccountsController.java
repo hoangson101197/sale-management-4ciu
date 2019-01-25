61825453d90b601d2afba9c3c004a8dc81d20701
sonnh <hoangson.101197@gmail.com>
2019-01-25 11:30:32 +0700

    gắn danh sách các account và role vào tất cả các model của view để sử dụng ở view

--- src/main/java/com/codegym/fourciumanagerprocesssale/controller/AccountsController.java
+++ src/main/java/com/codegym/fourciumanagerprocesssale/controller/AccountsController.java
@@ -1,16 +1,39 @@
 package com.codegym.fourciumanagerprocesssale.controller;
 
+import com.codegym.fourciumanagerprocesssale.model.Role;
+import com.codegym.fourciumanagerprocesssale.model.User;
+import com.codegym.fourciumanagerprocesssale.service.AccountsService;
+import com.codegym.fourciumanagerprocesssale.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("admin")
 public class AccountsController {
+
+    @Autowired
+    private AccountsService accountsService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @ModelAttribute("roles")
+    public Page<Role> roles(Pageable pageable) {
+        return roleService.findAll(pageable);
+    }
+
     @GetMapping("/accounts")
-    public ModelAndView AccountsList() {
+    public ModelAndView AccountsList(@PageableDefault(size = 5) Pageable pageable) {
+        Page<User> accounts = accountsService.findAll(pageable);
         ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("accounts", accounts);
         modelAndView.setViewName("admin/accounts");
         return modelAndView;
     }
